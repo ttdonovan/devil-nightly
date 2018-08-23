3fb57bf1a505c06f197d733bd6566df40e274b2f
Tanner Donovan <ttdonovan@gmail.com>
2018-08-22 23:43:13 -0700

    wip: working on setup of googletest gmock

---
+++ Test/devil_test.cpp
@@ -0,0 +1,21 @@
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+
+// #include "types.h"
+
+#include <windows.h>
+#include <dsound.h>
+
+#include "defs.h"
+// #include "enums.h"
+#include "structs.h"
+
+#include "Source/player.h"
+
+#if !defined(GTEST_CUSTOM_INIT_GOOGLE_TEST_FUNCTION_)
+
+TEST(Player, PlayerCppInit) {
+    // player_cpp_init();
+}
+
+#endif  // !defined(GTEST_CUSTOM_INIT_GOOGLE_TEST_FUNCTION_)
---
+++ Test/main.cpp
@@ -0,0 +1,5 @@
+#include "gmock/gmock.h"
+
+#if !defined(GTEST_CUSTOM_INIT_GOOGLE_TEST_FUNCTION_)
+    TEST(DummyTest, Dummy) {}
+#endif  // !defined(GTEST_CUSTOM_INIT_GOOGLE_TEST_FUNCTION_)
